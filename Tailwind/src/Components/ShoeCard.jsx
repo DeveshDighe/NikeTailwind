79a35d9653512dd28a858c77d46509a4c071aa0c
DeveshDighe <deveshdighe30@gmail.com>
2024-01-29 18:41:08 +0530

    fixing changing images

--- Tailwind/src/Components/ShoeCard.jsx
+++ Tailwind/src/Components/ShoeCard.jsx
@@ -9,7 +9,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigSheoImg }) => {
     return (
         <div className={`border-2 rounded-xl 
         ${bigSheoImg === imgURL.bigShoe ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`} onClick={handleClick}>
-            <div className=' flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+            <div className=' flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4 cursor-pointer'>
                 <img src={imgURL.thumbnail} alt="Shoe Collection" width={127} height={103} className=' object-contain' />
             </div>
         </div>
--- Tailwind/src/sections/Hero.jsx
+++ Tailwind/src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import Button from "../Components/Button";
 import {arrowRight} from '../assets/icons';
 import {shoes, statistics} from '../constants';
-import { bigShoe1} from "../assets/images";
+import { bigShoe1 ,bigShoe2, bigShoe3} from "../assets/images";
 import ShoeCard from "../Components/ShoeCard";
 import { useState } from "react";
 
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-10 ">
 
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 z-1">
         <p className=" text-xl font-montserrat text-coral-red">Our Summer Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold max-sm:mt-0">
           <span className=" xl:bg-white xl:whitespace-nowrap relative pr-10">The New Arrival</span>
@@ -31,7 +31,7 @@ const Hero = () => {
           </div>
       </div>
 
-      <div className=" relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center   -z-10">
+      <div className=" relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img className=" object-contain relative z-10" src= {bigShoeImg} alt="Shoe Collection" width={610} height={500} />
 
         <div className=" flex sm:gap-6 gap-4 absolute -bottom-[8%] sm:left-[10%] max-sm:px-6">
