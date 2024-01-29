728d08b1c23073d063763f6f04fc29d0d220ef0c
DeveshDighe <deveshdighe30@gmail.com>
2024-01-29 18:49:15 +0530

    Updates

--- Tailwind/src/sections/Hero.jsx
+++ Tailwind/src/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen max-container gap-10 ">
 
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 z-1">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 z-10 max-xl:-z-20">
         <p className=" text-xl font-montserrat text-coral-red">Our Summer Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold max-sm:mt-0">
           <span className=" xl:bg-white xl:whitespace-nowrap relative pr-10">The New Arrival</span>
